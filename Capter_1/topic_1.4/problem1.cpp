a156cfda5214e493b47ccd72a74c77f0a130eed5
uttom-kumar <uttom00781@gmail.com>
2026-02-04 10:44:14 +0600

    added topic4 and problem 1

---
+++ Capter_1/topic_1.4/problem1.cpp
@@ -0,0 +1,17 @@
+/*
+    Here is a program that contains both C and C++  style comments :
+ */
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int num; // this is a c++ single line comment
+
+    // Enter the integer number
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "You entered: " << num << endl;
+    return 0;
+}
