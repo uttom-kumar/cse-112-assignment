2cb704cc3c65034a03ef9a233357a4c82000884f
uttom-kumar <uttom00781@gmail.com>
2026-02-04 12:04:36 +0600

    problem 2 file added

--- Capter_1/topic_1.6/Examples/problem2.cpp
+++ Capter_1/topic_1.6/Examples/problem2.cpp
@@ -1,3 +1,28 @@
-//
-// Created by akash on 2/4/26.
-//
+/*
+    Here is a short program that illustrates how local variables can be declared anywhere within a block :
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Start of program" << endl;
+
+    int x = 10;  // local variable declared at the start
+    cout << "x = " << x << endl;
+
+    if (x > 5) {
+        int y = 20;  // local variable declared inside if block
+        cout << "y = " << y << endl;
+
+        // you can even declare a variable here
+        int z = x + y;
+        cout << "z = " << z << endl;
+    }
+
+    int a = 50; // variable declared later in the block
+    cout << "a = " << a << endl;
+
+    return 0;
+}
