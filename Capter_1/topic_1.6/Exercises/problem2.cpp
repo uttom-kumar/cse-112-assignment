4b0508d7d8be9250feecf8312b51587dff6b1c2f
uttom-kumar <uttom00781@gmail.com>
2026-02-05 12:29:06 +0600

    problem 2

---
+++ Capter_1/topic_1.6/Exercises/problem2.cpp
@@ -0,0 +1,38 @@
+/*
+    On your own, try declaring local variables at various points in a C++ program.
+    Try the same in a program, paying attention to which declarations generate errors.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 10; // valid
+    int i; // valid
+
+    for (i = 1; i <= num; i++)
+    {
+        cout << i << ", "<< endl;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
